8c5485a0a0ad8f15f0552b9e7e1b0dd222f703e5
johnny99457 <johnny99457@gmail.com>
2021-12-20 14:37:26 +0800

    nothing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Popular from '../pages/Popular';
-import Home from '../pages/Home';
-import Discover from '../pages/Discover';
-import Categories from '../pages/Categories';
-import Setting from '../pages/Setting';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Popular from "../pages/Popular";
+import Home from "../pages/Home";
+import Discover from "../pages/Discover";
+import Categories from "../pages/Categories";
+import Setting from "../pages/Setting";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/popular" element={<Popular />} />
-            <Route path="/discover" element={<Discover />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/setting" element={<Setting />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/popular" element={<Popular />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/setting" element={<Setting />} />
+    </Routes>
+  );
 };
 
 export default Router;
--- src/components/navtop/index.js
+++ src/components/navtop/index.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Tabs, Tab, Box, AppBar, Toolbar, InputBase } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { Link } from 'react-router-dom';
+import { Tabs, Tab, Box, AppBar, Toolbar, InputBase } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 const Navtop = () => {
-    const [value, setValue] = React.useState(0);
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" color="transparent">
-                <Toolbar>
-                    <Tabs value={value} textColor="inherit" onChange={handleChange}>
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          {/* <Tabs value={value} textColor="inherit" onChange={handleChange}>
                         <Tab label="Main" component={Link} to="/" />
                         <Tab label="Popular" component={Link} to="/popular" />
-                    </Tabs>
-                    <div>
-                        <SearchIcon />
-                        <InputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+                    </Tabs> */}
+          <div>
+            <SearchIcon />
+            <InputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navtop;
