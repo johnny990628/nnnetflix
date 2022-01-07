7e1afc1d40efb9426103b4ef94725505a03aacda
johnny990628 <johnny99457@gmail.com>
2022-01-07 16:24:14 +0800

    style:movie detail page

--- src/components/moviecard/index.js
+++ src/components/moviecard/index.js
@@ -1,105 +1,97 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import {
-  Box,
-  Card,
-  CardContent,
-  CardMedia,
-  Skeleton,
-  Typography,
-  Button,
-} from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Card, CardContent, CardMedia, Skeleton, Typography, Button } from '@mui/material';
 
-import { IMG_URL } from "../../api/api";
-import Modal from "../modal";
+import { IMG_URL } from '../../api/api';
+import Modal from '../modal';
 
 const styles = {
-  container: {
-    position: "relative",
-    maxWidth: "calc(100% - 1rem)",
-    display: "flex",
-    alignItems: "flex-end",
-    backgroundColor: "transparent",
-    transition: ".5s ease-out",
-    "&:hover": {
-      transform: "scale(1.02)",
-      opacity: 1,
-      cursor: "pointer",
-      "& .overlay": {
-        top: "0",
-      },
+    container: {
+        position: 'relative',
+        maxWidth: 'calc(100% - 1rem)',
+        display: 'flex',
+        alignItems: 'flex-end',
+        backgroundColor: 'transparent',
+        transition: 'all .5s ease-out',
+        '&:hover': {
+            transform: 'scale(1.02)',
+            opacity: 1,
+            cursor: 'pointer',
+            '& .overlay': {
+                top: '0',
+            },
+        },
     },
-  },
-  img: {
-    borderRadius: "2rem",
-  },
-  overlay: {
-    position: "absolute",
-    backgroundColor: "rgba(0,0,0,.7)",
-    width: "100%",
-    height: "100%",
-    color: "var(--text-color)",
-    borderRadius: "2rem",
-    textAlign: "center",
-    padding: "1rem",
+    img: {
+        borderRadius: '2rem',
+    },
+    overlay: {
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,.7)',
+        width: '100%',
+        height: '100%',
+        color: 'var(--text-color)',
+        borderRadius: '2rem',
+        textAlign: 'center',
+        padding: '1rem',
 
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  title: {
-    color: "var(--text-color)",
-    margin: ".5rem",
-  },
-  text: {
-    color: "var(--text-color)",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
-    textOverflow: "ellipsis",
-    margin: ".5rem",
-  },
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    title: {
+        color: 'var(--text-color)',
+        margin: '.5rem',
+    },
+    text: {
+        color: 'var(--text-color)',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        margin: '.5rem',
+    },
 };
 
 const MovieCard = ({ item }) => {
-  const [openModal, setOpenModal] = useState(false);
-  const [loading, setLoading] = useState(true);
+    const [openModal, setOpenModal] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-  const handleModal = (isOpen) => {
-    console.log(isOpen);
-    setOpenModal(isOpen);
-  };
+    const handleModal = (isOpen) => {
+        console.log(isOpen);
+        setOpenModal(isOpen);
+    };
 
-  return (
-    <Link to={`/movie/${item.id}`}>
-      <Card
-        sx={styles.container}
-        elevation={0}
-        // onClick={() => handleModal(true)}
-      >
-        <Box>
-          {/* {loading && <Skeleton sx={{ height: '35vw' }} />} */}
-          <CardMedia
-            component="img"
-            src={`${IMG_URL}${item.poster_path || item.backdrop_path}`}
-            alt={item.title}
-            loading="lazy"
-            sx={styles.img}
-            onLoad={() => {
-              setLoading(false);
-            }}
-          />
-          <Box className="overlay" sx={styles.overlay}>
-            <Typography variant="h5" sx={styles.title}>
-              {item.title}
-            </Typography>
-            <Typography sx={styles.text}>{item.release_date}</Typography>
-            <Typography sx={styles.text}>{item.overview}</Typography>
-          </Box>
-        </Box>
-        {/* <Modal open={openModal} handleModal={handleModal} /> */}
-      </Card>
-    </Link>
-  );
+    return (
+        <Link to={`/movie/${item.id}`}>
+            <Card
+                sx={styles.container}
+                elevation={0}
+                // onClick={() => handleModal(true)}
+            >
+                <Box>
+                    {/* {loading && <Skeleton sx={{ height: '35vw' }} />} */}
+                    <CardMedia
+                        component="img"
+                        src={`${IMG_URL}${item.poster_path || item.backdrop_path}`}
+                        alt={item.title}
+                        loading="lazy"
+                        sx={styles.img}
+                        onLoad={() => {
+                            setLoading(false);
+                        }}
+                    />
+                    <Box className="overlay" sx={styles.overlay}>
+                        <Typography variant="h5" sx={styles.title}>
+                            {item.title}
+                        </Typography>
+                        <Typography sx={styles.text}>{item.release_date}</Typography>
+                        <Typography sx={styles.text}>{item.overview}</Typography>
+                    </Box>
+                </Box>
+                {/* <Modal open={openModal} handleModal={handleModal} /> */}
+            </Card>
+        </Link>
+    );
 };
 
 export default MovieCard;
--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -1,37 +1,76 @@
-import { Box, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { makeStyles } from "@mui/styles";
-import { useParams } from "react-router-dom";
-import API, { IMG_URL } from "../api/api";
+import { Box, Grid, Typography, Chip, Divider } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { StarOutline } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+import { useParams } from 'react-router-dom';
+import API, { IMG_URL } from '../api/api';
 
 const useStyles = makeStyles((theme) => ({
-  img: {
-    width: "100%",
-  },
+    container: { padding: '2rem' },
+    img: {
+        width: '100%',
+        borderRadius: '2rem',
+    },
+    title: {
+        fontSize: '4rem',
+    },
+    tags: {
+        color: 'var(--text-color)',
+        marginRight: '1rem',
+        padding: '.5rem',
+        fontSize: '1rem',
+    },
+    text: { fontSize: '1.5rem' },
+    head: { fontSize: '3rem', marginTop: '1rem' },
 }));
 
 const MovieInfo = () => {
-  const classes = useStyles();
-  const { movieID } = useParams();
-  const [movie, setMovie] = useState({});
-  useEffect(async () => {
-    const result = await API.getMovieInfo(movieID);
-    setMovie(result);
-  }, [movie]);
-  return (
-    <Grid container spacing={4}>
-      <Grid item xs={4}>
-        <img
-          src={`${IMG_URL}${movie.poster_path || movie.backdrop_path}`}
-          alt={movie.title}
-          className={classes.img}
-        />
-      </Grid>
-      <Grid item xs={8}>
-        {movie.title}
-      </Grid>
-    </Grid>
-  );
+    const classes = useStyles();
+    const { movieID } = useParams();
+    const [movie, setMovie] = useState({});
+    useEffect(async () => {
+        const result = await API.getMovieInfo(movieID);
+        setMovie(result);
+    }, []);
+    return (
+        <Grid container spacing={4} className={classes.container}>
+            <Grid item xs={4}>
+                <Box>
+                    <img
+                        src={`${IMG_URL}${movie.poster_path || movie.backdrop_path}`}
+                        alt={movie.title}
+                        className={classes.img}
+                    />
+                </Box>
+            </Grid>
+            <Grid item xs={8}>
+                <Box>
+                    <Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Typography className={classes.title}>{movie.title}</Typography>
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                <StarOutline sx={{ marginRight: '.5rem', color: '#F0B734' }} />
+                                <Typography className={classes.text}>{movie.vote_average}</Typography>
+                            </Box>
+                        </Box>
+                        {movie.genres &&
+                            movie.genres.map((genre) => {
+                                return (
+                                    <Chip
+                                        label={<Typography className={classes.text}>{genre.name}</Typography>}
+                                        variant="outlined"
+                                        className={classes.tags}
+                                    />
+                                );
+                            })}
+
+                        <Typography className={classes.head}>電影概要</Typography>
+                        <Typography className={classes.text}>{movie.overview || '無相關電影概要'}</Typography>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default MovieInfo;
